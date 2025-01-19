79f4bef0ac0eb95ad05b746c30f13319f36b1d58
John <mackvb@yahoo.com>
2025-01-19 14:06:36 -0700

    VanDetails
    Add van details

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans"
+import VanDetail from "./pages/VanDetail"
+
+import "./server";
 
 export default function App() {
   return (
@@ -12,9 +16,12 @@ export default function App() {
           </Link>
           <nav>
             <Link to="/about">About</Link>
+            <Link to="/vans">Vans</Link>
           </nav>
         </header>
         <Routes>
+          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/vans" element={<Vans />} />
           <Route path="/about" element={<About />} />
           <Route path="/" element={<Home />} />
         </Routes>
