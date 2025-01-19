1befb6439baa75caefd219af6b2d73f27d06a3d9
John <mackvb@yahoo.com>
2025-01-19 12:46:11 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <header>
+          <Link className="site-logo" to="/">
+            #VanLife
+          </Link>
+          <nav>
+            <Link to="/about">About</Link>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
