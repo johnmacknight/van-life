ec6a18d97732c31a426f94e15d0ee031018ff4d7
John <mackvb@yahoo.com>
2025-01-21 10:38:02 -0700

    Host pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout"
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans/Vans"
 import VanDetail from "./pages/Vans/VanDetail"
+import HostLayout from "./components/HostLayout"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
-import Layout from "./components/Layout"
-import HostLayout from "./components/HostLayout"
+import HostVans from "./pages/Host/HostVans"
+import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
 
 import "./server";
 
@@ -30,6 +35,12 @@ export default function App() {
               <Route index element={<Dashboard />} />
               <Route path="income" element={<Income />} />
               <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo/>} />
+                <Route path="pricing" element={<HostVanPricing/>} />
+                <Route path="photos" element={<HostVanPhotos/>} />
+              </Route>
             </Route>
           </Route>
         </Routes>
--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,13 +1,52 @@
 import React from "react"
-import { Link, Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom"
 
 export default function HostLayout() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+
+    /*
+        Note: all of the paths here are relative to the /host directory 
+        so instead of /host/income
+        we can just use income
+
+        Also the .  just means the current directory aka. /host
+    */
     return (
         <>
             <nav className="host-nav">
-                <Link to="/host">Dashboard</Link>
-                <Link to="/host/income">Income</Link>
-                <Link to="/host/reviews">Reviews</Link>
+                <NavLink
+                    to="."
+                    end
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Dashboard
+                </NavLink>
+
+                <NavLink
+                    to="income"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Income
+                </NavLink>
+                
+                <NavLink
+                    to="vans"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Vans
+                </NavLink>
+
+                <NavLink
+                    to="reviews"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Reviews
+                </NavLink>
+
             </nav>
             <Outlet />
         </>
